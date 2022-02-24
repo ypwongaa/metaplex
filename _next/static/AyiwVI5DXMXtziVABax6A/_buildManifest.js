fba95a9e1bf4fe24110ed6d6e622455ed9ed09b1
ypwongaa <ypwongaa@connect.ust.hk>
2022-02-24 21:09:36 +0800

    Updates

---
+++ _next/static/AyiwVI5DXMXtziVABax6A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-292552b0e2f4eb088665.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
